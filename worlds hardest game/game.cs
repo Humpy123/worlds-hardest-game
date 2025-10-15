20bbfee684cabff27f6cb9f50928ba10664735d7
Henkebollen <henry.gussich@gmail.com>
2025-10-15 13:42:38 +0200

    game

--- worlds hardest game/game.cs
+++ worlds hardest game/game.cs
@@ -14,14 +14,14 @@ namespace worlds_hardest_game
             this.board = board;
         public void Run()
         {
-            while (!board.IsGameOver)
+            while ()
             {
                 board.Print();
                 var key = Console.ReadKey(true).Key;
-                if (key == ConsoleKey.UpArrow) board.MoveSnake(0, -1);
-                else if (key == ConsoleKey.DownArrow) board.MoveSnake(0, 1);
-                else if (key == ConsoleKey.LeftArrow) board.MoveSnake(-1, 0);
-                else if (key == ConsoleKey.RightArrow) board.MoveSnake(1, 0);
+                if (key == ConsoleKey.UpArrow) board.MovePlayer(0, -1);
+                else if (key == ConsoleKey.DownArrow) board.MovePlayer(0, 1);
+                else if (key == ConsoleKey.LeftArrow) board.MovePlayer(-1, 0);
+                else if (key == ConsoleKey.RightArrow) board.MovePlayer(1, 0);
             }
             board.Print();
 
