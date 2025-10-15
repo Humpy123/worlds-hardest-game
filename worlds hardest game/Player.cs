50b862eb370db455a2e8e2a4ca152c245311f1b5
Henkebollen <henry.gussich@gmail.com>
2025-10-15 13:24:32 +0200

    player

---
+++ worlds hardest game/Player.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace worlds_hardest_game
+{
+    public class Player
+    {
+        private int x, y;
+
+        public char Symbol { get; set; }
+
+        public Player(int x, int y,char symbol)
+        {
+            this.x = x;
+            this.y = y;
+            this.Symbol = symbol;
+        }
+
+        public void Move(int dx, int dy)
+        {
+            this.x += dx;
+            this.y += dy;
+
+        }
+    }
+}
