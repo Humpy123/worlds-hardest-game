a390d642ee01858a889c2b51f15a0547c05efccf
Henkebollen <henry.gussich@gmail.com>
2025-10-17 10:55:24 +0200

    IEnemyFactory

---
+++ worlds hardest game/IEnemyFactory.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace worlds_hardest_game
+{
+    interface IEnemyFactory
+    {
+        ICharacter CreateEnemy(int x, int y, char symbol, Board board);
+    }
+
+    class BasicEnemyFactory : IEnemyFactory
+    {
+        public ICharacter CreateEnemy(int x, int y, char symbol, Board board) 
+        {
+            return new BasicEnemy(x, y, symbol, new UpAndDownMovement(board));
+        }
+    }
+
+}
+
